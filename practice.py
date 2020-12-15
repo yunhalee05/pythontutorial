0237d739caac80054ae6fffe187b2f725dea1400
yunhalee05 <yunhalee0506@naver.com>
2020-12-15 18:41:51 +0900

    Pythontutorial

--- practice.py
+++ practice.py
@@ -320,4 +320,94 @@ print("--conglaturation--")
 
 
 #if
-  
+# weather = input("오늘 날씨는 어때요?")
+# if(weather =="비" or weather=="눈"):
+#     print("우산을 챙기세요")
+# elif weather =="미세먼지":
+#     print("마스크를 챙기세요")
+# else :
+#     print("준비물 필요 없어요.")
+# #여기서 Input은 항상 문자로 받기 때문에 int로 변환하려면 따로 변환 해줘야 한다.
+# temp = int(input("기온은 어때요?"))
+# if 30<=temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10<=temp and temp <30:
+#     print("괜찮은 날씨에요")
+# elif 0<=temp and temp<10: #= 0<=temp<10
+#     print("외투를 챙기세요")
+# else:
+#     print("추워요. 나가지 마세요")
+
+#for 똑같은 문장 여러번 (중복)
+# for waiting_no in[0,1,2,3,4]:
+#     print("대기번호 :{0}".format(waiting_no))
+
+# for waiting_no2 in range(5): #0,1,2,3,4
+#     print("대기번호 :{0}".format(waiting_no2))
+
+# for waiting_no2 in range(1,6): #1,2,3,4,5
+#     print("대기번호 :{0}".format(waiting_no2))
+
+# starbucks = ["아이언맨","토르","아이엠그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+
+#while
+# customer ="토르"
+# index = 5
+# while index>=1:
+#     print("{0},커피가 준비되었습니다. {1}번 남았어요.".format(customer,index))
+#     index-=1
+#     if index ==0:
+#         print("커피는 폐기처분 되었습니다. ")
+
+#무한 루프
+# customer = "아이언맨"
+# while True:
+#     print("{0},커피가 준비되었습니다. 호출 {1}회.".format(customer,index))
+#     index +=1
+
+#손님이름이 맞으면 탈출
+# customer = "토르"
+# person = "unknown"
+# while person !=customer :
+#     print("{0},커피가 준비되었습니다.".format(customer))
+#     person = input("이름이 어떻게 되세요?")
+        
+#continue, break
+absent = [2,5] #결석
+no_book = [7] #책을 안가져옴
+for student in range(1,11): #1~10
+    if student in absent:#만약 부른 학생이 결석이라면
+        continue#결석학생은 스킵한다.
+    elif student in no_book:
+        print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(no_book))
+        break #여기서 수업 종료 (반복문 탈출)
+    print("{0}, 책을 읽어봐".format(student))
+
+#한줄 for 출석번호 앞에 100을 붙이기로 했다. 
+students =[1,2,3,4,5]
+print (students)
+students = [i+100 for i in students]
+print(students)
+#학생 이름을 길이로 변환
+students2 = ["iron man","thor", "I am groot"]
+students2 = [len(i) for i in students2]
+print(students2)
+#학생 이름을 대문자로
+students3 = ["iron man","thor", "I am groot"]
+students3 = [i.upper() for i in students3]
+print(students3)
+
+#퀴즈 택시기사 1명, 50명의 승객과 매칭 기회 , 총 탑승 승객 수를 구하는 프로그램
+#승객별 소요시간은 5~50분 사이의 난수, 소요시간 5~15사이의 승객만 매칭해야 한다. 
+from random import *
+cnt  =0
+for i in range(1,51):
+    time = randrange(5,51)#5~50분 사이의 난수
+    if 5<=time<=15:#5~15분 손님 승객수 증가,매칭 성공
+        print("[0] {0}번째 손님 (소요시간:{1}분)".format(i,time))
+        cnt +=1
+    else : #매칭 실패
+        print("[] {0}번째 손님 (소요시간 :{1}분".format(i,time))
+print("총 탑승 승객 :{0}분".format(cnt))
